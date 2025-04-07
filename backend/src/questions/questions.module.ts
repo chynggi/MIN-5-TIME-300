927df888697406dd64ef903e93d54dde8d7f2ebf
chynggi <chynggi69@gmail.com>
2025-04-08 08:49:31 +0900

    fix

--- backend/src/questions/questions.module.ts
+++ backend/src/questions/questions.module.ts
@@ -3,11 +3,12 @@ import { QuestionsService } from './questions.service';
 import { QuestionsController } from './questions.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { UsersModule } from '../users/users.module';
+import { QuestionRepository } from './question.repository'
 
 @Module({
   imports: [PrismaModule, UsersModule],
   controllers: [QuestionsController],
-  providers: [QuestionsService],
+  providers: [QuestionsService, QuestionRepository],
   exports: [QuestionsService],
 })
 export class QuestionsModule {}
--- frontend/app/community/new/page.tsx
+++ frontend/app/community/new/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { communityService } from '@/lib/api/community';
 import styles from '@/styles/Community.module.css';
 
