1ba7f86065296c5ecccb743919d191b063803340
chynggi <chynggi69@gmail.com>
2025-09-04 20:19:04 +0900

    upgrade

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+   images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'chynggi.cafe24.com',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
   async rewrites() {
     return [
       {
